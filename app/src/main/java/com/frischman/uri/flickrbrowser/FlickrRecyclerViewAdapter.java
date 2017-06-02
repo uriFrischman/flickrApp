0c4cccbabaf12190c6bc0cb169a5cf12906fb3a4
Uri <uri.frischman@gmail.com>
2017-06-02 18:57:00 -0400

    images load and listeners work

---
+++ app/src/main/java/com/frischman/uri/flickrbrowser/FlickrRecyclerViewAdapter.java
@@ -0,0 +1,76 @@
+package com.frischman.uri.flickrbrowser;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+/**
+ * Created by Uri on 2017-06-02.
+ */
+
+class FlickrRecyclerViewAdapter extends RecyclerView.Adapter<FlickrRecyclerViewAdapter.FlickrImageViewHolder> {
+
+    private static final String TAG = "FlickrRecyclerViewAdapt";
+    private List<Photo> mPhotosList;
+    private Context mContext;
+
+    public FlickrRecyclerViewAdapter(List<Photo> photosList, Context context) {
+        mPhotosList = photosList;
+        mContext = context;
+    }
+
+    @Override
+    public FlickrImageViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        Log.d(TAG, "onCreateViewHolder: new view requested");
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.browse, parent, false);
+        return new FlickrImageViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(FlickrImageViewHolder holder, int position) {
+        Photo photoItem = mPhotosList.get(position);
+        Log.d(TAG, "onBindViewHolder: " + photoItem.getTitle() + "------> " +  position);
+        Picasso.with(mContext).load(photoItem.getImage())
+                .error(R.drawable.placeholder)
+                .placeholder(R.drawable.placeholder)
+                .into(holder.thumbnail);
+        holder.title.setText(photoItem.getTitle());
+    }
+
+    @Override
+    public int getItemCount() {
+        return ((mPhotosList != null) && (mPhotosList.size() != 0) ? mPhotosList.size() : 0);
+    }
+
+    void loadNewData(List<Photo> newPhotos) {
+        mPhotosList = newPhotos;
+        notifyDataSetChanged();
+    }
+
+    public Photo getPhoto(int position) {
+        return ((mPhotosList != null) && (mPhotosList.size() != 0) ? mPhotosList.get(position) : null);
+    }
+
+    static class FlickrImageViewHolder extends RecyclerView.ViewHolder {
+        private static final String TAG = "FlickrImageViewHolder";
+        ImageView thumbnail = null;
+        TextView title = null;
+
+        public FlickrImageViewHolder(View itemView) {
+            super(itemView);
+            Log.d(TAG, "FlickrImageViewHolder: starts");
+            this.thumbnail = (ImageView) itemView.findViewById(R.id.thumbnail);
+            this.title = (TextView) itemView.findViewById(R.id.title);
+        }
+    }
+    
+}
--- app/src/main/java/com/frischman/uri/flickrbrowser/MainActivity.java
+++ app/src/main/java/com/frischman/uri/flickrbrowser/MainActivity.java
@@ -2,16 +2,22 @@ package com.frischman.uri.flickrbrowser;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements GetFlickrJsonData.OnDataAvailable {
+public class MainActivity extends AppCompatActivity implements GetFlickrJsonData.OnDataAvailable, RecyclerItemClickListener.OnRecyclerClickListener {
 
     private static final String TAG = "MainActivity";
+    private FlickrRecyclerViewAdapter mFlickrRecyclerViewAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +26,15 @@ public class MainActivity extends AppCompatActivity implements GetFlickrJsonData
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        recyclerView.addOnItemTouchListener(new RecyclerItemClickListener(this, recyclerView, this));
+
+        mFlickrRecyclerViewAdapter = new FlickrRecyclerViewAdapter(new ArrayList<Photo>(), this);
+        recyclerView.setAdapter(mFlickrRecyclerViewAdapter);
+
         Log.d(TAG, "onCreate: ends");
 
 //        GetRawData getRawData = new GetRawData(this);
@@ -61,14 +76,25 @@ public class MainActivity extends AppCompatActivity implements GetFlickrJsonData
 
     @Override
     public void onDataAvailable(List<Photo> data, DownloadStatus status) {
+        Log.d(TAG, "onDataAvailable: starts");
         if (status == DownloadStatus.OK) {
+            mFlickrRecyclerViewAdapter.loadNewData(data);
             Log.d(TAG, "onDataAvailable: data is " + data);
         } else {
             Log.e(TAG, "onDataAvailable: failed with status " + status);
         }
+        Log.d(TAG, "onDataAvailable: ends");
     }
 
+    @Override
+    public void onItemClick(View view, int position) {
+        Log.d(TAG, "onItemClick: starts");
+        Toast.makeText(MainActivity.this, "Normal tap position " + position, Toast.LENGTH_SHORT).show();
+    }
 
-
-
+    @Override
+    public void OnItemLongClick(View view, int position) {
+        Log.d(TAG, "OnItemLongClick: starts");
+        Toast.makeText(MainActivity.this, "Long tap at position " + position, Toast.LENGTH_SHORT).show();
+    }
 }
